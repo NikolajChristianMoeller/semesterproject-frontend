e9f154b9fdf98f7897119e58aa874190577edb69
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2023-11-29 13:53:51 +0100

    dev stuff

--- src/components/AdminTable.jsx
+++ src/components/AdminTable.jsx
@@ -1,43 +1,43 @@
-export default function AdminTable({products, deleteClicked, updateClicked}){
-
-    return(
-        <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Produkt</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Options</th>
-          </tr>
-        </thead>
-        <tbody>
-        {products.map((product)=>(
+export default function AdminTable({ products, deleteClicked, updateClicked }) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Produkt</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Options</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
           <tr key={product.ID}>
             <th scope="row">{product.ID}</th>
             <td>{product.Name}</td>
             <td>{product.Stock}</td>
             <td>
-                <button className="btn btn-danger rounded-pill" 
-                        type="button" 
-                        data-bs-toggle="modal" 
-                        data-bs-target="#delete-modal"
-                        onClick={()=>deleteClicked(product)}
-                        >
-                    Delete
-                </button>
-                <button className="btn btn-info ms-2 rounded-pill" 
-                        type="button"
-                        data-bs-toggle="modal" 
-                        data-bs-target="#update-modal"
-                        onClick={()=>updateClicked(product)}
-                >
-                    Update
-                </button>
+              <button
+                className="btn btn-danger rounded-pill"
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#delete-modal"
+                onClick={() => deleteClicked(product)}
+              >
+                Delete
+              </button>
+              <button
+                className="btn btn-info ms-2 rounded-pill"
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#update-modal"
+                onClick={() => updateClicked(product)}
+              >
+                Update
+              </button>
             </td>
           </tr>
         ))}
-        </tbody>
-      </table>
-    )
-
+      </tbody>
+    </table>
+  );
 }
--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -3,57 +3,96 @@
 // TODO: add dropdown with collection
 // TODO: setup so you can add multiple collections and colors when updating
 
-export default function UpdateForm({productToUpdate}){
-
-    return(
-        <div className="modal" id="update-modal" tabIndex="-1" aria-labelledby="update-modal" aria-hidden="true">
-        <div className="modal-dialog modal-fullscreen" >
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="update-modal-label">Update {productToUpdate.Name} ({productToUpdate.ID})</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
+export default function UpdateForm({ productToUpdate }) {
+  return (
+    <div
+      className="modal"
+      id="update-modal"
+      tabIndex="-1"
+      aria-labelledby="update-modal"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-fullscreen">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="update-modal-label">
+              Update {productToUpdate.Name} ({productToUpdate.ID})
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
             <form className="row g-3">
-                <div className="col-md-5">
-                    <label htmlFor="productName" className="form-label">Product Name</label>
-                    <input type="text" className="form-control" id="productName"/>
-                </div>
-                <div className="col-md-5">
-                    <label htmlFor="productPrice" className="form-label">Price</label>
-                    <input type="text" className="form-control" id="productPrice"/>
-                </div>
-                <div className="col-10">
-                    <label htmlFor="productDescription" className="form-label">Description</label>
-                    <textarea type="text" className="form-control" id="productDescription" rows="6" />
-                </div>
-                <div className="col-md-5">
-                    <label htmlFor="inputCollection" className="form-label">Collection(s)</label>
-                    <select id="inputCollection" className="form-select">
-                    <option>Choose...</option>
-                    <option>...</option>
-                    </select>
-                </div>
-                <div className="col-md-5">
-                    <label htmlFor="inputColor" className="form-label">Color(s)</label>
-                    <select id="inputColor" className="form-select">
-                    <option>Choose...</option>
-                    <option>...</option>
-                    </select>
-                </div>
-                <div className="col-12">
-                    <button type="submit" className="btn btn-primary">Sign in</button>
-                </div>
-                </form>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-success" data-bs-dismiss="modal">Update</button>
-            </div>
+              <div className="col-md-5">
+                <label htmlFor="productName" className="form-label">
+                  Product Name
+                </label>
+                <input type="text" className="form-control" id="productName" />
+              </div>
+              <div className="col-md-5">
+                <label htmlFor="productPrice" className="form-label">
+                  Price
+                </label>
+                <input type="text" className="form-control" id="productPrice" />
+              </div>
+              <div className="col-10">
+                <label htmlFor="productDescription" className="form-label">
+                  Description
+                </label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="productDescription"
+                  rows="6"
+                />
+              </div>
+              <div className="col-md-5">
+                <label htmlFor="inputCollection" className="form-label">
+                  Collection(s)
+                </label>
+                <select id="inputCollection" className="form-select">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="col-md-5">
+                <label htmlFor="inputColor" className="form-label">
+                  Color(s)
+                </label>
+                <select id="inputColor" className="form-select">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary">
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              className="btn btn-success"
+              data-bs-dismiss="modal"
+            >
+              Update
+            </button>
           </div>
         </div>
       </div>
-
-    )
-
+    </div>
+  );
 }
