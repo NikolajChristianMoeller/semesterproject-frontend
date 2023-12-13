6a42ff49a1e851159eafb4b60a1636cf20ac5c4b
Blue-Grizzly <emilnissen2@gmail.com>
2023-12-13 11:24:32 +0100

    verceljson tweak

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -73,12 +73,12 @@ export default function Checkout({ cart, fillCart, reduceCart, emptyCart }) {
               </div>
             </div>
             <button className="btn btn-outline-secondary">
-            <Link to={"/"} style={{textDecoration: "none", color:"black"}}>
+            <p onClick={()=>navigate("/")}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
               </svg>
                 Tilbage til butikken 
-               </Link>
+               </p>
             </button>
               <Feedback/>
           </div>
